0d38d846c7d6e4e974bb176eb5f378251f3a3829
romain.deveaud <romain.deveaud@f2872f82-f626-11dd-a8e2-f7cb3c35fcff>
2009-02-09 13:55:47 +0000

    Début du projet, script permettant le chargement des librairies.

---
+++ test.rb
@@ -0,0 +1,49 @@
+#
+# All the lines below load the libraries
+linkparserpath = "/home/ubuntu/trashInstall/linkparser/trunk/"
+wordnetpath = "/home/ubuntu/trashInstall/wordnet/wordnet-0.0.5/"
+$LOAD_PATH.unshift( linkparserpath+"lib", linkparserpath+"ext" )
+
+begin
+	puts "Requiring 'linkparser' module..."
+	require 'linkparser'
+rescue => e
+	$stderr.puts "LinkParser module failed to load : #{e.message}\n\t" +
+		e.backtrace.join( "\n\t" )
+end
+
+$dict = LinkParser::Dictionary.new
+$LOAD_PATH.unshift(wordnetpath+"lib")
+
+begin
+	puts "Requiring 'wordnet' module..."
+	require 'wordnet'
+rescue => e
+	$stderr.puts "WordNet failed to load : #{e.message}\n\t" +
+		e.backtrace.join( "\n\t" )
+end
+
+$lex = WordNet::Lexicon.new("/usr/share/ruby-wordnet")
+
+begin
+puts "Requiring 'linguistics'..."
+require 'linguistics'
+rescue => e
+    $stderr.puts "Linguistics failed to load : #{e.message}\n\t" +
+        e.backtrace.join( "\n\t" )
+end
+
+puts "\nCalling Linguistics::EN module..."
+include Linguistics::EN
+#
+# End of loading stuff
+
+puts "\nIs linkparser correctly loaded? => "+Linguistics::EN.has_link_parser?.to_s
+puts "Is wordnet correctly loaded? => "+Linguistics::EN.has_wordnet?.to_s
+
+print "Enter your question : "
+question = gets
+sent = sentence(question)
+puts sent.diagram
+puts sent.linkages.first.constituent_tree_string
+puts sent.linkages.first.links_and_domains
