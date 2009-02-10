585de58adaac7762d044fedc39dd96f27884715f
romain.deveaud <romain.deveaud@f2872f82-f626-11dd-a8e2-f7cb3c35fcff>
2009-02-10 20:39:05 +0000

    Les extractions marchent globalement bien pour le premier moteur.

--- trunk/lib/request.rb
+++ trunk/lib/request.rb
@@ -10,6 +10,15 @@ class Request
     end
 
     def get_np
+        np = []
+        @sent.linkages.first.links.each { |l| np.push(l.lword, l.rword) if l.label == "G" }
+        np.uniq!
+        if np.length > 2
+            t = np.shift
+            np.insert(1,t)
+            np.reverse!
+        end
+        np
     end
 
 # Les deux fonctions ci-dessous vont analyser la requête pour choisir
@@ -19,6 +28,7 @@ class Request
     end
 
     def extract
+        print "Keywords selected for engine 1 : "
         extract_e1
     end
 
@@ -30,13 +40,17 @@ private
     end
 
     def extract_e1
-        np = Array.new
-        @sent.linkages.first.links.each { |l| np.push(l.lword, l.rword) if l.label == "G" }
-        np.uniq!
-        proper_noun = ""
-        np.each { |w| proper_noun += "#{w.split(".").first} " }
-        proper_noun.strip
-        proper_noun
+        kw_array = get_np
+        if !get_np.include?(@sent.object)
+            kw_array.push(@sent.object)
+            @sent.linkages.first.links.each do |l|
+                kw_array.push(l.lword) if (l.rword.split(".").first == @sent.object) && (l.label =~ /A.*/)
+            end
+        end
+        kw_str = "" 
+        kw_array.each { |w| kw_str += "#{w.split(".").first} " }
+        kw_str.strip
+        kw_str
     end
     
     def extract_e2
