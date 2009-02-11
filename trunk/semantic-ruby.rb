01b11054eb08e0983c9f619831724fa5d2dab4c0
romain.deveaud <romain.deveaud@f2872f82-f626-11dd-a8e2-f7cb3c35fcff>
2009-02-11 13:25:32 +0000

    Nouvelle extraction des mots-clés fonctionnelle, reste plus qu'à trouver une liste de stopwords et ça sera bon.

--- trunk/semantic-ruby.rb
+++ trunk/semantic-ruby.rb
@@ -5,10 +5,22 @@
 
 require 'lib/loading'
 require 'lib/request'
+require 'readline'
 
-while true do
-    print "Enter your request : "
-    str = gets.chomp!
+def prompt(prompt)
+# Fonction permettant de gérer l'historique de saisie des requêtes
+# ainsi que le déplacement avec les flèches gauches et droites.
+    input = nil
+    prompt += " " unless prompt =~ /\s$/
+    loop do
+        input = Readline.readline(prompt, true)
+        break if input.length > 0
+    end
+    input
+end
+
+loop do
+    str = prompt("Enter your request : ")
     break if str == "exit" # Condition de sortie
     request = Request.new(str)
     puts request.extract
