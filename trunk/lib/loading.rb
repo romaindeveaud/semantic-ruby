ae278fd186bb4710fb0bea712bae5d370c76bf63
romain.deveaud <romain.deveaud@f2872f82-f626-11dd-a8e2-f7cb3c35fcff>
2009-04-14 23:23:04 +0000

    Modifications de fichiers

--- trunk/lib/loading.rb
+++ trunk/lib/loading.rb
@@ -3,9 +3,9 @@
 linkparserpath = ENV['PWD']+"/.semantic-rubyInstall/linkparser"
 wordnetpath = ENV['PWD']+"/.semantic-rubyInstall/wordnet-0.0.5"
 linguisticspath = ENV['PWD']+"/.semantic-rubyInstall/linguistics"
+$LOAD_PATH.unshift( linkparserpath+"/lib" , linkparserpath+"/ext" )
 $LOAD_PATH.unshift(wordnetpath+"/lib")
 $LOAD_PATH.unshift(linguisticspath+"/lib")
-$LOAD_PATH.unshift( linkparserpath+"/lib" , linkparserpath+"/ext" )
 
 
 begin
