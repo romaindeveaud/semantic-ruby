b04de8276eaeb460f06e86cf4b5c0b119880f0a1
romain.deveaud <romain.deveaud@f2872f82-f626-11dd-a8e2-f7cb3c35fcff>
2009-04-14 22:27:25 +0000

    Nouveau script d'installation

--- install.sh
+++ install.sh
@@ -1,63 +1,45 @@
-rm -rf ~/.semantic-rubyInstall ;
-sudo apt-get update ;
-sudo apt-get install subversion ruby irb rdoc libc6-dev libopenssl-ruby ruby1.8-dev tcl-dev tk-dev libdb4.2 libdb4.2-dev ;
-cd ~ ;
-mkdir .semantic-rubyInstall ;
-cd .semantic-rubyInstall ;
-echo "Installing RubyGems 1.3.1..." ; 
-wget http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz ;
-tar xzf rubygems-1.3.1.tgz ;
-cd rubygems-1.3.1 ;
-sudo ruby setup.rb ;
-sudo gem1.8 install rake rdoc rcov rspec diff-lcs activesupport ;
-cd .. ; 
-mkdir linkparser ; 
-cd linkparser ;
-svn co svn://deveiate.org/Ruby-LinkParser2/trunk . ;
-tar xzf link-grammar-4.4.1.tar.gz ;
-cd link-grammar-4.4.1 ;
-./configure ; make ;
-sudo make install ;
-cd .. ;
-echo "Installing LinkParser..." ;
-export LD_LIBRARY_PATH=/usr/local/lib ;
-sudo rake ; sudo rake install ;
-cd .. ;
-wget http://www.deveiate.org/code/wordnet-0.0.5.tar.gz ;
-wget http://wordnet.princeton.edu/3.0/WordNet-3.0.tar.gz ;
-wget ftp://moulon.inra.fr/pub/ruby/bdb-0.6.5.tar.gz ;
-tar xzf wordnet-0.0.5.tar.gz ;
-tar xzf WordNet-3.0.tar.gz ;
-tar xzf bdb-0.6.5.tar.gz ;
-cd WordNet-3.0 ;
-./configure ; make ;
-sudo make install ;
-cd ../bdb-0.6.5 ;
-ruby extconf.rb ;
+#!/bin/bash
+
+echo "-- Semantic-ruby installer --"
+if [ ! -r .semantic-rubyInstall ]
+then 
+  echo "\t **Error : unable to find directory .semantic-rubyInstall/ \n\tPlease launch the installer in the semantic-ruby root directory."
+  exit
+fi
+
+echo -n "updating sources... "
+apt-get update
+echo "done"
+
+echo -n "installing ruby essentials... "
+apt-get install ruby-full build-essential
+echo "done"
+
+echo -n "installing rubygems... "
+apt-get install rubygems
+echo "done"
+
+echo -n "creating symbolic links... "
+ln -s $PWD/.semantic-rubyInstall/linkparser/link-grammar-4.4.1/link-grammar/.libs/liblink-grammar.so.4 /usr/lib/liblink-grammar.so.4
+ln -s $PWD/.semantic-rubyInstall/wordnet-0.0.5/ruby-wordnet/ /usr/share/ruby-wordnet
+echo "done"
+
+echo -n "installing libdb4.2-dev... "
+apt-get install libdb4.2-dev
+echo "done"
+
+echo -n "installing activesupport... "
+gem install activesupport
+echo "done"
+
+echo -n "installing link-grammar... "
+cd .semantic-rubyInstall/linkparser/link-grammar-4.4.1/ ;
+./configure ;
 make ;
-sudo make install ;
-cd ../wordnet-0.0.5 ;
-echo -e "\033[31;1m ==== Press 'y' when prompted first time, and 'Enter' when prompted second time ==== \033[0m" ;
-ruby convertdb.rb ;
-sudo rake ;
-sudo rake install ;
-cd /usr/share ;
-sudo chmod -R og+rx ruby-wordnet ;
-sudo cp ~/.semantic-rubyInstall/wordnet-0.0.5/ruby-wordnet/* ruby-wordnet ;
-sudo mkdir ruby-wordnet ;
-cd ~/.semantic-rubyInstall ;
-mkdir linguistics ; cd linguistics ;
-svn co svn://deveiate.org/Linguistics/trunk . ;
-sudo rake ;
-sudo rake install ;
-echo "export LPPATH=/home/$USER/.semantic-rubyInstall/linkparser" >> /home/$USER/.bashrc ;
-echo "export WNPATH=/home/$USER/.semantic-rubyInstall/wordnet-0.0.5" >> /home/$USER/.bashrc ;
-echo "export LANG=en_US.UTF-8" >> /home/$USER/.bashrc ;
-echo "export LD_LIBRARY_PATH=/usr/local/lib" >> /home/$USER/.bashrc ;
-export LPPATH=/home/$USER/.semantic-rubyInstall/linkparser ;
-export WNPATH=/home/$USER/.semantic-rubyInstall/wordnet-0.0.5 ;
-export LANG=en_US.UTF-8 ;
-cd .. ;
-rm -f *.tar.gz *.tgz ;
-cd .. ;
-sudo chmod -R og+rx .semantic-rubyInstall ;
+make install ;
+echo "done"
+
+echo -n "installing bdb-0.6.5... "
+cd ../../bdb-0.6.5 ;
+make install ;
+echo "done"
