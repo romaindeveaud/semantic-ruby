a876023716c41a431af280703da38efa4af1d6b0
romain.deveaud <romain.deveaud@f2872f82-f626-11dd-a8e2-f7cb3c35fcff>
2009-05-08 14:23:11 +0000

    Version online de NLGbAse.QR combinant les moteurs 1 et 2

--- www/nlgbaseqr.rb
+++ www/nlgbaseqr.rb
@@ -12,8 +12,30 @@ cgi.out() do
 #    cgi.body() do
       if cgi.params.has_key?("request")
         sock.print cgi.params["request"]
-        results = sock.recvfrom(1000000)[0]+"<br /><a href='http://sit.vickev.fr/semantic-ruby/www/'>Back home</a>"
-        results
+        results = sock.recvfrom(1000000)[0]#+"<br /><a href='http://sit.vickev.fr/semantic-ruby/www/'>Back home</a>"
+#        results
+        "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">
+        <html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'> 
+          <head> 
+            <meta http-equiv='content-type' content='text/html;charset=UTF-8' /> 
+            <title>NLGbAse.QR</title> 
+            <style type='text/css' media='screen'>
+                <!-- @import url( http://www.nlgbase.org/model.css ); -->
+            </style>
+
+          </head> 
+          <body> 
+          <div id='header'>
+          <h1><a href='http://www.nlgbase.org'>NLGbAse.QR</a></h1>
+
+          </div>
+          <a href='http://www.nlgbase.org'> </a>
+          <div class='pages'>
+          <ul>
+          <li class='current_page_item'><a href='http://sit.vickev.fr/semantic-ruby/www/'>Home</a></li>
+          <li class='page_item page-item-10'><a href='http://www.nlgbase.org/'>NLGbAse</a></li>
+          </ul>
+          </div>#{results}</td></tr></table></pre></body></html>"
       else
         CGI::escapeHTML("Failure !")
       end
--- www/semantic-ruby_server.rb
+++ www/semantic-ruby_server.rb
@@ -4,12 +4,14 @@ if ENV['PWD'].split("/").last != "semantic-ruby"
   puts " **Error : the server must be launch from the main semantic-ruby directory."
   exit
 end
-$LOAD_PATH.unshift(ENV['PWD']+"/lib")
+$LOAD_PATH.unshift(ENV['PWD']+"/lib", ENV['PWD']+"/www")
 
 require 'loading'
 require 'request'
 require 'readline'
 require 'socket'
+require 'rexml/document'
+require 'xmllistener'
 
 serv = TCPServer.new(12005)
 puts " ** NLGbAse.QR server is running... ** "
@@ -26,7 +28,16 @@ loop do
       result += "[#{results[:cat_e2]}] #{results[:kw_e2]}<br />"
       result += "Keywords selected for engine 3 : "
       result += "[#{results[:cat_e3]}] [Named entity : #{results[:en_e3]}] [Keywords : #{results[:kw_e3]}]<br />"
-      res = Net::HTTP.get(URI.parse("http://www.nlgbase.org/perl/info_extractor.pl?query=#{results[:kw_e1].split.join("+")}&search=EN"))
+      res1 = Net::HTTP.get(URI.parse("http://www.nlgbase.org/perl/rr_info_extractor_xml.pl?ename=#{results[:cat_e2]}&terms=#{results[:kw_e1].split.join("+")}&search=EN"))
+      listener = XMLlistener.new
+      parser = Parsers::StreamParser.new(res1, listener)
+      parser.parse
+      res = "<br /><big><span style='font-weight: bold; margin-left : 50px;'>Semantic search, basic</span></big>#{listener.get_doc}"
+      res2 = Net::HTTP.get(URI.parse("http://www.nlgbase.org/perl/rr_info_extractor_xml.pl?query=#{results[:kw_e1].split.join("+")}&search=EN"))
+      listener = XMLlistener.new
+      parser = Parsers::StreamParser.new(res2, listener)
+      parser.parse
+      res += "<br /><big><span style='font-weight: bold; margin-left : 50px;'>Simple search</span></big>#{listener.get_doc}"
     rescue NoMethodError,URI::InvalidURIError 
       result+= "Execution error. Maybe your sentence has not been written properly."
     end
